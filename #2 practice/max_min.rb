0a25fbef5e138070f9f432de5cc7e24e54a4b185
ilsemakin <48153238+ilsemakin@users.noreply.github.com>
2019-03-02 15:55:06 +0300

    Update max_min.rb

--- #2 practice/max_min.rb
+++ #2 practice/max_min.rb
@@ -3,7 +3,7 @@ def input_number(message)
         print "\nInput number #{message} > "
         input = gets
         return nil if input.nil?
-        return Float(input)
+        return Float(input.strip)
         rescue 
             puts "Error! Please, repeat!"
     end
