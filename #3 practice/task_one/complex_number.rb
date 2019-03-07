828e306cb3e6e849a8e80a2aa05a9a6b7533a2cc
ilsemakin <48153238+ilsemakin@users.noreply.github.com>
2019-03-07 21:53:33 +0300

    Create complex_number.rb

---
+++ #3 practice/task_one/complex_number.rb
@@ -0,0 +1,33 @@
+class MyComplex
+
+  attr_reader :real, :imaginary
+
+  def initialize(real, imaginary)
+    @real = Float(real)
+    @imaginary = Float(imaginary)
+  end
+
+  def add(other)
+    MyComplex.new(@real + other.real, @imaginary + other.imaginary)
+  end
+
+  def sub(other)
+    MyComplex.new(@real - other.real, @imaginary - other.imaginary)
+  end
+
+  def multiply(other)
+    MyComplex.new(@real*other.real - @imaginary*other.imaginary, @real*other.imaginary + @imaginary*other.real)
+  end
+
+  def devide(other)
+    re = @real*other.real + @imaginary*other.imaginary
+    im = @real*(-other.imaginary) + @imaginary*other.real
+    denomitor = (other.real)**2 + (other.imaginary)**2
+  
+    MyComplex.new(re/denomitor, im/denomitor)
+  end
+
+  def to_s
+    "#{@real} + i*#{@imaginary}"
+  end
+end
