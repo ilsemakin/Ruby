1020be00bb2cfa9e9f434529aa3869e5c4a9a04b
Ilya Semakin <il.semakin@github.com>
2019-04-13 20:05:15 +0300

    corrected code

--- #3 practice/task_one/complex_number.rb
+++ #3 practice/task_one/complex_number.rb
@@ -1,5 +1,7 @@
-class MyComplex
+# frozen_string_literal: true
 
+# create complex number
+class MyComplex
   attr_reader :real, :imaginary
 
   def initialize(real, imaginary)
@@ -16,15 +18,15 @@ class MyComplex
   end
 
   def multiply(other)
-    MyComplex.new(@real*other.real - @imaginary*other.imaginary, @real*other.imaginary + @imaginary*other.real)
+    MyComplex.new(@real * other.real - @imaginary * other.imaginary, @real * other.imaginary + @imaginary * other.real)
   end
 
   def devide(other)
-    re = @real*other.real + @imaginary*other.imaginary
-    im = @real*(-other.imaginary) + @imaginary*other.real
-    denomitor = (other.real)**2 + (other.imaginary)**2
-  
-    MyComplex.new(re/denomitor, im/denomitor)
+    re = @real * other.real + @imaginary * other.imaginary
+    im = @real * -other.imaginary + @imaginary * other.real
+    denomitor = other.real**2 + other.imaginary**2
+
+    MyComplex.new(re / denomitor, im / denomitor)
   end
 
   def to_s
