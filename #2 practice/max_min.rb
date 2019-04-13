61e4f3e655d82e1f62734317a44850e7c614f639
Ilya Semakin <il.semakin@github.com>
2019-04-13 19:17:52 +0300

    corrected code

--- #2 practice/max_min.rb
+++ #2 practice/max_min.rb
@@ -1,38 +1,41 @@
+# frozen_string_literal: true
+
 def input_number(message)
-    loop do
-        print "\nInput number #{message} > "
-        input = gets
-        return nil if input.nil?
-        return Float(input)
-        rescue 
-            puts "Error! Please, repeat!"
-    end
+  loop do
+    print "\nInput number #{message} > "
+    input = gets
+
+    return nil if input.nil?
+
+    number = Float(input)
+    puts "Result : #{number}"
+    return number
+
+  rescue ArgumentError => _exception
+    puts 'Error! Try again!'
+  end
 end
 
 def min(first, second)
-    return first unless first > second
-    return second
+  return first unless first > second
+
+  second
 end
 
 def max(first, second)
-    return first if first > second
-    return second
-end 
+  return first if first > second
+
+  second
+end
 
 def main
-    a = input_number("a")
-    print "Output: #{a}\n"
+  number_a = input_number('a')
+  number_b = input_number('b')
+  number_c = input_number('c')
+  number_d = input_number('d')
 
-    b = input_number("b")
-    print "Output: #{b}\n"
-    
-    c = input_number("c")
-    print "Output: #{c}\n"
-    
-    d = input_number("d")
-    print "Output: #{d}\n"
-
-    puts "\nmax[min(a,b), min(c,d)] = #{max(min(a,b), min(c,d))}"
+  output = "\nmax[min(#{number_a}, #{number_b}), min(#{number_c}, #{number_d})] = "
+  puts output + max(min(number_a, number_b), min(number_c, number_d)).to_s
 end
 
 main
