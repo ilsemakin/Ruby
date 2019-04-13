878f8205ea8aae9fa85e316a8c47479551b21fe9
Ilya Semakin <il.semakin@github.com>
2019-04-13 16:06:25 +0300

    corrected code

--- #1 practice/input_output.rb
+++ #1 practice/input_output.rb
@@ -1,8 +1,10 @@
-string = "stop, please"
-while true
-    print ">"
-    line = gets
-    break if line.nil?
-    break if string == line.strip
-    puts line
+string = 'stop, please'
+
+loop do
+  print '> '
+  line = gets
+  break if line.nil?
+  break if string == line.strip
+
+  puts line
 end
