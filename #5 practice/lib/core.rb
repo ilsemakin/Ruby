9adbc5a30c38ce31102ced6afa6ea492bba09dce
Ilya Semakin <il.semakin@github.com>
2019-04-14 12:38:15 +0300

    start program

--- #5 practice/lib/core.rb
+++ #5 practice/lib/core.rb
@@ -1,5 +1,41 @@
+# frozen_string_literal: true
+
+require_relative 'dao'
+require_relative 'input'
+
+# start program
 class Core
+  def initialize
+    dao = Dao.new
+    @database = dao.read_db
+    @commands = { 0 => 'help', 1 => 'show', 2 => 'exit' }
+    help
+  end
+
   def run
-    print "> "
+    input = Input.new
+    loop do
+      case input.choose(@commands)
+      when 0
+        help
+      when 1
+        show
+      when 2
+        exit
+      end
+    end
+  end
+
+  def show
+    @database.courses.each do |course|
+      puts "\n#{course.name}"
+      course.each do |name, grades|
+        puts "Student ID : #{name} - Grade : #{grades}"
+      end
+    end
+  end
+
+  def help
+    @commands.each { |key, command| puts "#{key} -> #{command}" }
   end
 end
