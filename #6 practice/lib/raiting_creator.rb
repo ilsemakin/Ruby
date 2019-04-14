63353dccad518876f1834fa4a929c39ae665fe66
Ilya Semakin <il.semakin@github.com>
2019-04-15 00:20:51 +0300

    core program

---
+++ #6 practice/lib/raiting_creator.rb
@@ -0,0 +1,22 @@
+
+require_relative 'movie_list'
+
+# core program
+class RatingCreator
+    def create
+        arg = ARGV
+        error('number agrs') if arg.size < 2
+        error('file no exist') unless File.exist?(arg[0]) || File.exist?(arg[1])
+        
+        movie_list = MovieList.new
+        movie_list.read_data(arg[0])
+        movie_list.save_sorted_list(arg[1])
+    end
+
+    private
+    
+    def error(message)
+        puts message
+        exit
+    end
+end
