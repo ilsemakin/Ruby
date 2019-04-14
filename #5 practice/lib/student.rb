37b173ceb456c2f7294a003ceddd80ea69996e91
Ilya Semakin <il.semakin@github.com>
2019-04-14 12:31:03 +0300

    fixed by rubocop

--- #5 practice/lib/student.rb
+++ #5 practice/lib/student.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
+# creating one student
 class Student
   attr_accessor :id, :name, :surname
 
   def initialize(id, name, surname)
     @id = id
     @name = name
-    @surname = surname 
+    @surname = surname
   end
 end
