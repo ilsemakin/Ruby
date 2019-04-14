cdc65e9dc983afd180cab8a2e493e29589a9b229
Ilya Semakin <il.semakin@github.com>
2019-04-15 00:21:52 +0300

    processing list of movies

---
+++ #6 practice/lib/movie_list.rb
@@ -0,0 +1,23 @@
+
+require 'csv'
+require_relative 'movie'
+
+# creating list of movies
+class MovieList
+    def initialize
+        @movies = []
+    end
+
+    def read_data(file)
+        CSV.foreach(file, headers: true) do |row|
+            movie = Movie.new(row['title'], row['kinopoisk'], row['imdb'], row['metacritic'], row['rotten_tomatoes'])
+            @movies.push(movie)    
+        end
+    end
+
+    def save_sorted_list(file)
+        File.open(file, 'w') do |file| 
+            @movies.sort!.each {|movie| file.write("#{movie} | #{movie.ogon_rating}\n")}
+        end
+    end
+end
