6d9573b47c8c9a048e5c9f70cca0787b5e1d96ac
Ilya Semakin <il.semakin@github.com>
2019-04-15 01:09:36 +0300

    fixed by rubocop

--- #6 practice/lib/movie_list.rb
+++ #6 practice/lib/movie_list.rb
@@ -1,23 +1,24 @@
+# frozen_string_literal: true
 
 require 'csv'
 require_relative 'movie'
 
 # processing list of movies
 class MovieList
-    def initialize
-        @movies = []
-    end
+  def initialize
+    @movies = []
+  end
 
-    def read_data(file)
-        CSV.foreach(file, headers: true) do |row|
-            movie = Movie.new(row['title'], row['kinopoisk'], row['imdb'], row['metacritic'], row['rotten_tomatoes'])
-            @movies.push(movie)    
-        end
+  def read_data(file_input)
+    CSV.foreach(file_input, headers: true) do |row|
+      movie = Movie.new(row['title'], row['kinopoisk'], row['imdb'], row['metacritic'], row['rotten_tomatoes'])
+      @movies.push(movie)
     end
+  end
 
-    def save_sorted_list(file)
-        File.open(file, 'w') do |file| 
-            @movies.sort!.each {|movie| file.write("#{movie} | #{movie.ogon_rating}\n")}
-        end
+  def save_sorted_list(file_output)
+    File.open(file_output, 'w') do |file|
+      @movies.sort!.each { |movie| file.write("#{movie} | #{movie.ogon_rating}\n") }
     end
+  end
 end
--- #6 practice/lib/raiting_creator.rb
+++ #6 practice/lib/raiting_creator.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
 
 require_relative 'movie_list'
 
 # core program
 class RatingCreator
-    def create
-        arg = ARGV
-        error('Invalid number of arguments!') if arg.size < 2
-        error('File no exist!') unless File.exist?(arg[0]) && File.exist?(arg[1])
-        
-        movie_list = MovieList.new
-        movie_list.read_data(arg[0])
-        movie_list.save_sorted_list(arg[1])
-    end
+  def create
+    arg = ARGV
+    error('Invalid number of arguments!') if arg.size < 2
+    error('File no exist!') unless File.exist?(arg[0]) && File.exist?(arg[1])
 
-    private
-    
-    def error(message)
-        puts message
-        exit
-    end
+    movie_list = MovieList.new
+    movie_list.read_data(arg[0])
+    movie_list.save_sorted_list(arg[1])
+  end
+
+  private
+
+  def error(message)
+    puts message
+    exit
+  end
 end
