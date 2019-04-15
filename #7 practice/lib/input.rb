15deaa11958c7650a4fa0051a92ee7eb3af05ce7
Ilya Semakin <il.semakin@github.com>
2019-04-16 00:57:49 +0300

    corrected code

--- #7 practice/lib/input.rb
+++ #7 practice/lib/input.rb
@@ -1,28 +1,30 @@
-
+# frozen_string_literal: true
 
 # input data
 module Input
-    def self.string(message)
-        loop do
-            print message
-            line = gets
-            return nil if line.nil?
-            return line.strip
-        end
-    end
+  def self.string(message)
+    loop do
+      print message
+      line = gets
+      return nil if line.nil?
+      return line.strip unless line.empty?
 
-    def self.number(message)
-        loop do
-            print message
-            line = gets
-            return nil if line.nil?
-
-            number = Integer(line.strip)
-            return number if number.pozitive?
-            
-            puts 'Number non pozitive! Try again!'
-        rescue ArgumentError => _exception
-            puts 'Input erroe! Try again!'
-        end
+      puts 'Line is empty! Try again!'
     end
+  end
+
+  def self.number(message)
+    loop do
+      print message
+      line = gets
+      return nil if line.nil?
+
+      number = Integer(line.strip)
+      return number if number.pozitive?
+
+      puts 'Number non pozitive! Try again!'
+    rescue ArgumentError => _exception
+      puts 'Input error! Try again!'
+    end
+  end
 end
