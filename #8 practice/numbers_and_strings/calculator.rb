b394f7c340daff5d24fd009420d54d27212d6ae5
Ilya Semakin <il.semakin@yandex.ru>
2019-04-17 10:19:35 +0300

    adding functions

---
+++ #8 practice/numbers_and_strings/calculator.rb
@@ -0,0 +1,51 @@
+
+
+# 1
+class Calculator
+  def calc
+    error('Error in numbers of arguments') if ARGV.size != 1
+
+    arg = ARGV[0].squeeze.split(' ')
+
+    denominator1 = arg[0].split('/')
+    denominator2 = arg[2].split('/')
+    check(denominator1, denominator2)
+
+    number1 = Rational(arg[0])
+    number2 = Rational(arg[2])
+    result = operations(number1, arg[1], number2)
+
+    puts result
+    puts Float(result)
+  rescue ArgumentError => _exception
+    puts 'Error in arguments'
+  end
+
+  def error(message)
+    puts message
+    exit
+  end
+
+  def check(denominator1, denominator2)
+    error('Error! Divided by zero!') if denominator1[1] == '0' || denominator2[1] == '0'
+  end
+
+  def operations(number1, arg, number2)
+    case arg
+    when '*'
+      number1 * number2
+    when '/'
+      error('Error! Divided by zero!') if Integer(number2).zero?
+      number1 / number2
+    when '+'
+      number1 + number2
+    when '-'
+      number1 - number2
+    else
+      error('Error in operation')
+    end
+  end
+end
+
+calculator = Calculator.new
+calculator.calc
